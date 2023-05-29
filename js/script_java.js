d565e7cc5062029c056ca0931ec36d12a7bd1124
pgprogithub <134913674+pgprogithub@users.noreply.github.com>
2023-05-29 15:41:12 +0200

    Add files via upload

---
+++ js/script_java.js
@@ -0,0 +1,26 @@
+
+    
+    function Hello(){
+        // var fond_bod=document.getElementById("fond_body");
+        var coleur=prompt("Votre couleur péférée");
+            
+            document.getElementById("fond_body").style.backgroundColor=coleur;
+          
+    }
+
+    // function fond(){
+        
+    //     alert("hh");
+    // }
+//    lien.addEventListener("change",function(){
+
+//     if(lien.value){
+//         document.getElementById("fond_body").style.backgroundImage="url('lien')";
+//     }
+//     else{
+//         alert("Choiser une image");
+//     }
+//    });
+    // function ChangeFond(){
+        
+    // }
